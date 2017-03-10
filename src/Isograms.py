33faf4af5b443e2e4dccec24efc71056b9fe2c47
Dustin Mayeda <dmayeda2000@gmail.com>
2017-03-10 11:21:05 -0800

    committing Isograms.py

---
+++ src/Isograms.py
@@ -0,0 +1,22 @@
+"""
+Description:
+
+An isogram is a word that has no repeating letters, consecutive or non-consecutive.
+Implement a function that determines whether a string that contains only letters is
+an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+is_isogram("Dermatoglyphics" ) == true
+is_isogram("aba" ) == false
+is_isogram("moOse" ) == false # -- ignore letter case
+"""
+
+def is_isogram(string):
+    string = string.lower()
+    z = True
+    for char in string:
+        if string.count(char) > 1:
+            return False
+        else:
+            z = True
+    if z == True:
+        return True
